1da81bd6214f9cd044ec33c6a38e514366b00d23
Mohammed Ehsan <mehsankhan02@gmail.com>
2026-02-13 20:18:55 +0400

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 from goal_generator import generate_snail_goal
 from parser import read_puzzle_from_file
 from checker import is_solvable
-
+from state import PuzzleState
 import pprint
+
+start = [
+    [1, 2, 3],
+    [4, 0, 5],
+    [6, 7, 8]
+]
+
+ps = PuzzleState(start)
+neighbors = ps.get_neighbors()
+
+for n in neighbors:
+    print(n.board)
+
+
 
 def main():
     # TODO: Parse CLI args
@@ -18,7 +32,7 @@ if __name__ == "__main__":
     main()
 
 
-puzzle = read_puzzle_from_file("example_4x4.txt")
-print("Solvable?" , is_solvable(puzzle))
+# puzzle = read_puzzle_from_file("example_4x4.txt")
+# print("Solvable?" , is_solvable(puzzle))
 
 # pprint.pprint(generate_snail_goal(4))
